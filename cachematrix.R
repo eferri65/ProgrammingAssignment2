c2ef2a6c95faedb1dddd0d4dd1adde20cbf46e75
eferri65 <elenarodellaferri@gmail.com>
2015-04-26 09:10:27 -0400

    added my tets

--- cachematrix.R
+++ cachematrix.R
@@ -44,6 +44,49 @@ cacheSolve <- function(x, ...) {
         m <- solve(matrix, ...)
         ## save the inverse of the matrix in the cache
         x$setinversematrix(m)
-        ## return the inverse the matrix
+        ## return the inverse of the matrix
         m
 }
+
+## my tests
+## setwd("ProgrammingAssignment2")
+## source("cachematrix.R")
+
+## -- test 1 : cacheSolve does NOT compute the inverse because the inverse is in the cache --
+## x<-matrix(c(4,2,7,6),nrow=2,ncol=2)
+## inversex<-solve(x)
+## specialMatrix<-makeCacheMatrix()
+## specialMatrix$set(x)
+## specialMatrix$get()
+## -- Result: you should get 
+## [,1] [,2]
+## [1,]    4    7
+## [2,]    2    6
+## --SETTING THE CACHE
+## specialMatrix$setinverse(inversex)
+## specialMatrix$getinverse()
+## -- Result: you should get
+## [,1] [,2]
+## [1,]  0.6 -0.7
+## [2,] -0.2  0.4
+## --cachesolve should "see" the cache already contains the inverse of the matrix x
+## cacheSolve(specialMatrix)
+## -- Result: you should get
+## getting cached data
+## [,1] [,2]
+## [1,]  0.6 -0.7
+## [2,] -0.2  0.4
+
+## -- test 2: cacheSolve DOES compute the inverse because the cache is empty --
+## x<-matrix(c(3,3.5,3.2,3.6),nrow=2,ncol=2)
+## specialMatrix<-makeCacheMatrix()
+## specialMatrix$set(x)
+## specialMatrix$get()
+## specialMatrix$getinverse()
+## -- Result: the cache is empty and you should get
+## NULL
+## cacheSolve(specialMatrix)
+## -- Result: you should get
+## [,1] [,2]
+## [1,] -9.00  8.0
+## [2,]  8.75 -7.5
